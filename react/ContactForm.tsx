7ab2e58f1f931f47f578e3266dcc636751766ab2
Dante Souza e Souza <lokinmodar@gmail.com>
2021-08-22 23:06:17 -0300

    fixinig naming in interface

--- react/ContactForm.tsx
+++ react/ContactForm.tsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
+import { useCssHandles } from 'vtex.css-handles'
 import axios from 'axios'
 
 import formValidation from './formValidation'
 
 import './styles.css'
 
+const CSS_HANDLES = [
+  'inputName',
+  'inputEmail',
+  'inputButton',
+  'container',
+  'formLeads',
+  'labelName',
+  'labelEmail',
+] as const
+
 const ContactForm = (): JSX.Element => {
   const [inputs, setInputs] = useState({ name: '', email: '' })
+  const handles = useCssHandles(CSS_HANDLES)
 
   const handleSubmit = async (e: {
     preventDefault: () => void
@@ -16,13 +28,24 @@ const ContactForm = (): JSX.Element => {
 
     if (!validation) return
 
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    }
+
     window.localStorage.setItem('Name', JSON.stringify(inputs.name))
     window.localStorage.setItem('E-mail', JSON.stringify(inputs.email))
     await axios
-      .post(`https://1841m6ur62.execute-api.sa-east-1.amazonaws.com/api`, {
-        name: inputs.name,
-        email: inputs.email,
-      })
+      .post(
+        `https://1841m6ur62.execute-api.sa-east-1.amazonaws.com/api`,
+        {
+          name: inputs.name,
+          email: inputs.email,
+        },
+        options
+      )
       .then((/* res */) => {
         // console.log(res.data)
       })
@@ -44,14 +67,17 @@ const ContactForm = (): JSX.Element => {
   }
 
   return (
-    <article>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">
-          Insira seu endereço de e-mail para saber mais:
+    <article className={`${handles.container}`}>
+      <form onSubmit={handleSubmit} className={`${handles.formLeads}`}>
+        <label htmlFor="name" className={`${handles.labelName}`}>
+          Quer se manter informado sobre nossas promoções?
+          <br />
+          Preencha os dados abaixo e clique em Enviar!
           <br />
           <input
-            type="email"
+            type="text"
             name="name"
+            className={`${handles.inputName}`}
             placeholder="Digite seu nome completo:"
             onChange={handleInputChange}
             value={inputs.name}
@@ -61,12 +87,13 @@ const ContactForm = (): JSX.Element => {
             required
           />
         </label>
-        <label htmlFor="email">
-          Insira seu endereço de e-mail para saber mais:
+        <label htmlFor="email" className={`${handles.labelEmail}`}>
           <br />
+
           <input
             type="email"
             name="email"
+            className={`${handles.inputEmail}`}
             placeholder="Digite seu endereço de E-mail:"
             onChange={handleInputChange}
             value={inputs.email}
@@ -76,7 +103,9 @@ const ContactForm = (): JSX.Element => {
             required
           />
         </label>
-        <button type="submit">Enviar!</button>
+        <button type="submit" className={`${handles.inputButton}`}>
+          Enviar!
+        </button>
       </form>
     </article>
   )
